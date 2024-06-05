5f894bf171e8b1d4df552b3c6c8dd77b0cd77120
Lucasvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-06-05 08:23:18 -0300

    atividade atualizada com o professor icaro

--- ativ-arrow/arrow.js
+++ ativ-arrow/arrow.js
@@ -8,47 +8,46 @@ As arrow functions não possuem a palavra-chave “function” , também permite
 /* ----------------------------------------------------*/
 
 // Exemplo basico
+
 const arrowFunction = (exemplo1, exemplo2) => {
-    // corpo da função
-    return exemplo;
-  };
-
-  /* ----------------------------------------------------*/
-
-  // 1)
-  const dobrarNumeros = (numeros) => {
-    return numeros.map(numero => numero * 2);
-  };
-  
-  // Exemplo na pratica
-  const numeros = [1, 2, 3, 4, 5];
-  const numerosDobrados = dobrarNumeros(numeros);
-  console.log(numerosDobrados); 
-
-  /* ----------------------------------------------------*/
-
-  // 2)
-  const ordenarPorComprimento = (palavras) => {
-    return palavras.sort((a, b) => a.length - b.length);
-  };
-  
-  // Exemplo na pratica
-  const palavras = ["banana", "maçã", "abacaxi", "uva", "laranja"];
-  const palavrasOrdenadas = ordenarPorComprimento(palavras);
-  console.log(palavrasOrdenadas); 
+  return exemplo;
+};
 
 /* ----------------------------------------------------*/
 
-  // 3)
+// 1)
+const dobrarNumeros = (numeros) => {
+  return numeros.map(numero => numero * 2);
+};
 
-  const filtrarNumerosPares = (numeros) => {
-    return numeros.filter(numero => numero % 2 === 0);
-  };
-  
-  // Exemplo na pratica
-  const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const numerosPares = filtrarNumerosPares(numeros2);
-  console.log(numerosPares); 
-  
+// Exemplo na pratica
+const numeros = [1, 2, 3, 4, 5];
+const numerosDobrados = dobrarNumeros(numeros);
+console.log(numerosDobrados); 
 
-  
+/* ----------------------------------------------------*/
+
+// 2)
+const ordenarPorComprimento = (palavras) => {
+  return palavras.sort((a, b) => a.length - b.length);
+};
+
+// Exemplo na pratica
+const palavras = ["4444", "1", "55555", "22", "333"];
+const palavrasOrdenadas = ordenarPorComprimento(palavras);
+console.log(palavrasOrdenadas); 
+
+/* ----------------------------------------------------*/
+
+// 3)
+
+const filtrarNumerosPares = (numeros) => {
+  return numeros.filter(numero => numero % 2 === 0);
+};
+
+// Exemplo na pratica
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosPares = filtrarNumerosPares(numeros2);
+console.log(numerosPares); 
+
+
