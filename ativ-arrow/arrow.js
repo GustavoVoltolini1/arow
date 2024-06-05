8948f2359193a6d5c284112b5d9bce0476179381
Lucasvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-06-05 09:11:42 -0300

    text

--- ativ-arrow/arrow.js
+++ ativ-arrow/arrow.js
@@ -7,10 +7,13 @@ As arrow functions não possuem a palavra-chave “function” , também permite
 /* ----------------------------------------------------*/
 
 // 1)
+/* Primeiro agente defini a função dobrarNumeros que recebe numeros, depois faz o "return" que retorna onde cada numero foi dobrado*/
+
 const dobrarNumeros = (numeros) => {
   return numeros.map(numero => numero * 2);
 };
 
+/*Declara a função numeros e incia o array de numeros e guarda dentro da "dobrarNumeros"*/
 // Executar as atividades
 function executar1() {
   const numeros = [1, 2, 3, 4, 5];
@@ -19,11 +22,13 @@ function executar1() {
 }
 /* ----------------------------------------------------*/
 
+/*Defini a função "ordenarPorComprimento"que recebe "palavras", depois o "return palavras.sort" ordena pelo tamanho da palavra*/
 // 2)
 const ordenarPorComprimento = (palavras) => {
   return palavras.sort((a, b) => a.length - b.length);
 };
 
+/*Declara a variavel palavra começa um array, depois a função "ordenarPorComprimento" recebe "palavras", e ordena as palavras de acordo com seu comprimento*/
 // Executar as atividades
 function executar2() {
   const palavras = ["4444", "1", "55555", "22", "333"];
@@ -33,11 +38,12 @@ function executar2() {
 
 /* ----------------------------------------------------*/
 
+/*Declara a função "filtrarNumerosPares" que recebe numeros, e retorna um novo array com numeros pares*/
 // 3)
-
 const filtrarNumerosPares = (numeros) => {
   return numeros.filter(numero => numero % 2 === 0);
 };
+
 
 // Executar as atividades
 function executar3() {
