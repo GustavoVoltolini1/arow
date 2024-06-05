8d155b2aa5a93933386ce5c7aeaa40ac8da17f80
Gustavovoltolini1 <gustavo_d_voltolini@estudante.sesisenai.org.br>
2024-06-05 08:49:29 -0300

    ajustes

--- ativ-arrow/arrow.js
+++ ativ-arrow/arrow.js
@@ -4,15 +4,6 @@
 
 As arrow functions não possuem a palavra-chave “function” , também permitem ter um retorno implícito, que são valores retornados sem ter que usar a palavra return.*/
 
-
-/* ----------------------------------------------------*/
-
-// Exemplo basico
-
-const arrowFunction = (exemplo1, exemplo2) => {
-  return exemplo;
-};
-
 /* ----------------------------------------------------*/
 
 // 1)
@@ -20,11 +11,12 @@ const dobrarNumeros = (numeros) => {
   return numeros.map(numero => numero * 2);
 };
 
-// Exemplo na pratica
-const numeros = [1, 2, 3, 4, 5];
-const numerosDobrados = dobrarNumeros(numeros);
-console.log(numerosDobrados); 
-
+// Executar as atividades
+function executar1() {
+  const numeros = [1, 2, 3, 4, 5];
+  const numerosDobrados = dobrarNumeros(numeros);
+  alert(numerosDobrados); 
+}
 /* ----------------------------------------------------*/
 
 // 2)
@@ -32,10 +24,12 @@ const ordenarPorComprimento = (palavras) => {
   return palavras.sort((a, b) => a.length - b.length);
 };
 
-// Exemplo na pratica
-const palavras = ["4444", "1", "55555", "22", "333"];
-const palavrasOrdenadas = ordenarPorComprimento(palavras);
-console.log(palavrasOrdenadas); 
+// Executar as atividades
+function executar2() {
+  const palavras = ["4444", "1", "55555", "22", "333"];
+  const palavrasOrdenadas = ordenarPorComprimento(palavras);
+  alert(palavrasOrdenadas); 
+}
 
 /* ----------------------------------------------------*/
 
@@ -45,9 +39,10 @@ const filtrarNumerosPares = (numeros) => {
   return numeros.filter(numero => numero % 2 === 0);
 };
 
-// Exemplo na pratica
-const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const numerosPares = filtrarNumerosPares(numeros2);
-console.log(numerosPares); 
-
+// Executar as atividades
+function executar3() {
+  const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const numerosPares = filtrarNumerosPares(numeros2);
+  alert(numerosPares); 
+}
 
